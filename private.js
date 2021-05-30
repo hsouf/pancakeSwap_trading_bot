020c66ba288736f1b6863f4d9da6d558f36a6843
Soufiane Hajazi <soufianehajazi23@gmail.com>
2021-05-30 20:19:36 +0100

    trading bot first commit

--- private.js
+++
@@ -1,5 +0,0 @@
-const ethers = require("ethers");
-let mnemonic =
-  "legend damage clinic resource birth maple avoid action clown female story invest";
-let mnemonicWallet = ethers.Wallet.fromMnemonic(mnemonic);
-console.log(mnemonicWallet.privateKey);
